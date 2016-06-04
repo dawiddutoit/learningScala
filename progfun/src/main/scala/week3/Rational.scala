62041aa8fbe102ae3ccfc982956d2fd8dacf7c49
dawiddutoit@gmail.com <dawiddutoit@gmail.com>
2016-06-04 22:23:51 +0200

    week 3 finishing rationals and basic methods with nice ways to write more verbose

--- progfun/src/main/scala/week3/Rational.scala
+++ progfun/src/main/scala/week3/Rational.scala
@@ -1,19 +1,35 @@
 package week3
 
 class Rational(x: Int, y: Int) {
-  def numer = x
+  require(y != 0, "denominator must be nonzero")
 
-  def denom = y
+  def this(x: Int) = this(x, 1)
 
-  def add(that: Rational) =
+  private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+
+  val numer = x
+
+  val denom = y
+
+  def <(that: Rational): Boolean = numer * that.denom < that.numer * denom
+
+  def max(that: Rational) = if (this < that) that else this
+
+  def +(that: Rational): Rational =
     new Rational(
-      numer * that.denom + that.numer * denom,
-      denom * that.denom
+      this.numer * that.denom + that.numer * this.denom,
+      this.denom * that.denom
     )
 
-  def neg: Rational = new Rational(-numer, denom)
 
-  def sub(that: Rational) = add(that.neg)
 
-  override def toString = numer + "/" + denom
+  def unary_- : Rational = new Rational(-numer, denom)
+
+  def sub(that: Rational) = this + -that
+
+
+  override def toString = {
+    val divisor = gcd(numer, denom)
+    numer / divisor + "/" + denom / divisor
+  }
 }
--- progfun/src/main/scala/week3/rationals.sc
+++ progfun/src/main/scala/week3/rationals.sc
@@ -5,9 +5,22 @@ object rationals {
   val x = new Rational(1, 3)
   val y = new Rational(5, 7)
   val z = new Rational(3, 2)
+
   x.numer
   x.denom
-  x.add(y)
+  x + y
+
+  val a = new Rational(10, 20)
+  a.numer
+  a.denom
 
   x.sub(y).sub(z)
+  y + y
+  x < y
+  x.max(y)
+
+  x + y
+//val strange = new Rational(1,0)
+//  strange.add(strange)
+  new Rational(2)
 }
